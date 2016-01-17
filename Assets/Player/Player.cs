e3e65af7e1f657c05db721df925f14787ccaf4a0
Maxime Kestemont <maxime.kestemont@realimpactanalytics.com>
2016-01-17 17:11:13 +0100

    RTS prototype

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RTS;
+
+public class Player : MonoBehaviour {
+
+	public HUD hud;
+	public string username;
+	public bool human;
+
+	public int startMoney, startMoneyLimit, startPower, startPowerLimit;
+	private Dictionary< ResourceType, int > resources, resourceLimits;
+
+	public WorldObject SelectedObject { get; set; }
+
+	// Use this for initialization
+	void Start () {
+		hud = GetComponentInChildren< HUD >();
+		
+		// Init resources
+		AddStartResourceLimits();
+		AddStartResources();
+	}
+	
+	void Awake() {
+    	resources = InitResourceList();
+    	resourceLimits = InitResourceList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( human ) {
+    		hud.SetResourceValues(resources, resourceLimits);
+		}
+	}
+
+	private Dictionary< ResourceType, int > InitResourceList() {
+    	Dictionary< ResourceType, int > list = new Dictionary< ResourceType, int >();
+    	list.Add(ResourceType.Money, 0);
+    	list.Add(ResourceType.Power, 0);
+    	return list;
+	}
+
+	private void AddStartResourceLimits() {
+		resourceLimits[ResourceType.Money] += startMoneyLimit;
+		resourceLimits[ResourceType.Power] += startPowerLimit;
+	}
+ 
+	private void AddStartResources() {
+		resources[ResourceType.Money] += startMoney;
+		resources[ResourceType.Power] += startPower;
+	}
+
+	public void AddResource(ResourceType type, int amount) {
+		resources[type] += amount;
+	}
+
+	public void AddUnit(string unitName, Vector3 spawnPoint, Vector3 rallyPoint, Quaternion rotation) {
+    	Debug.Log ("add " + unitName + " to player");
+
+		GameObject newUnit = (GameObject)Instantiate(ResourceManager.GetUnit(unitName), spawnPoint, rotation);
+		
+		// parent of unit = Units (list of all units of the player)
+		Units units = GetComponentInChildren< Units >();
+		newUnit.transform.parent = units.transform;
+
+		Unit unitObject = newUnit.GetComponent< Unit >();
+    	if(unitObject && spawnPoint != rallyPoint) unitObject.StartMove(rallyPoint);
+	}
+}
---
+++ Assets/RTS/ResourceManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+namespace RTS {
+    public static class ResourceManager {
+ 
+		public static float ScrollSpeed { get { return 25; } }
+		public static float RotateSpeed { get { return 100; } }
+		public static float RotateAmount { get { return 10; } }
+
+		// Speed to build units in queue
+		public static int BuildSpeed { get { return 1; } }
+		
+		// Width from which the camera can be moved (starting from the edge of the screen)
+		public static int ScrollWidth { get { return 15; } }
+
+		public static float MinCameraHeight { get { return 10; } }
+		public static float MaxCameraHeight { get { return 40; } }
+
+		private static Vector3 invalidPosition = new Vector3(-99999, -99999, -99999);
+		public static Vector3 InvalidPosition { get { return invalidPosition; } }
+
+		private static Bounds invalidBounds = new Bounds(new Vector3(-99999, -99999, -99999), new Vector3(0, 0, 0));
+		public static Bounds InvalidBounds { get { return invalidBounds; } }
+
+		// Resource box skin (stored here as it is the same for all objects)
+		private static GUISkin selectBoxSkin;
+		public static GUISkin SelectBoxSkin { get { return selectBoxSkin; }}
+		public static void StoreSelectBoxItems(GUISkin skin) {
+			selectBoxSkin = skin;
+		}
+
+		// GameObject list
+		private static GameObjectList gameObjectList;
+		public static void SetGameObjectList(GameObjectList objectList) {
+    		gameObjectList = objectList;
+		}
+
+		// Wrapper method // TODO are those really useful?
+		public static GameObject GetBuilding(string name) {
+    		return gameObjectList.GetBuilding(name);
+		}
+ 
+		public static GameObject GetUnit(string name) {
+    		return gameObjectList.GetUnit(name);
+		}
+ 
+		public static GameObject GetWorldObject(string name) {
+    		return gameObjectList.GetWorldObject(name);
+		}
+ 
+		public static GameObject GetPlayerObject() {
+    		return gameObjectList.GetPlayerObject();
+		}
+ 
+		public static Texture2D GetBuildImage(string name) {
+    		return gameObjectList.GetBuildImage(name);
+		}
+
+    }
+}
