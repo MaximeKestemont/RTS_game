b47036852ba794f893de5a5aabffa00fe7ce6bf0
Maxime Kestemont <maxime.kestemont@realimpactanalytics.com>
2016-02-20 15:44:53 +0100

    FIX fps display

--- Assets/Player/HUD/FPSDisplay.cs
+++ Assets/Player/HUD/FPSDisplay.cs
@@ -16,9 +16,9 @@ public class FPSDisplay : MonoBehaviour
  
 		GUIStyle style = new GUIStyle();
  
-		Rect rect = new Rect(w - 50, 20, w, h * 2 / 50);
+		Rect rect = new Rect(w - 100, 20, w, h * 2 / 50);
 		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 50;
+		style.fontSize = h * 2 / 70;
 		style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
 		float msec = deltaTime * 1000.0f;
 		float fps = 1.0f / deltaTime;
